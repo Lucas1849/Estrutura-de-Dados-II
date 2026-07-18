79ba4a1fe94392d95962a6216a10513132795222
Lucas1849 <lukinhas.spy6@gmail.com>
2026-07-17 23:08:54 -0300

    feat: mudança na forma como os livros são cadastrados. Agora a classe livro retorna suas informações para a documentação em um arquivo.txt. A função de cadastro de livro está funcionando como deve.

--- trabalhoed2/livro.py
+++ trabalhoed2/livro.py
@@ -4,3 +4,6 @@ class Livro:
         self.titulo = titulo
         self.autor = autor
         self.ano = ano
+        
+    def __str__(self):
+        return f'{self.codigo}; {self.titulo}; {self.autor}; {self.ano}'
--- trabalhoed2/main.py
+++ trabalhoed2/main.py
@@ -3,9 +3,25 @@ from livro import Livro
 
 def main():
     palavras = AvlTree()
-    caminho_do_arquivo = "trabalhoed2/texto_origem.txt"
-    insere_na_arvore(caminho_do_arquivo, palavras)
+    nome_documento_livros = 'trabalhoed2/acervo.txt' #Note, aqui a escolha txt é puramente pela natureza do trabalho e indicação no campo
+                                         # de entrega do moodle. Poderia ser um JSON, por exemplo.
+    documentar_livro(nome_documento_livros)
     
+    
+#Primeiro passo é preciso estruturar como as infomações dos livros serão estruturadas para que sejam cadastrados mais de um 
+#livro por vez
+def documentar_livro(arquivo_destino):
+    with open(arquivo_destino, 'w') as acervo:
+        while True:
+            quer_cadastrar = input("Deseja cadastrar um livro ? ").capitalize()[0]
+            if quer_cadastrar not in "Ss":
+                break
+            cadastro_livro = Livro(int(input("Código: ")),
+                                   str(input("Título: ")),
+                                   str(input("Autor: ")),
+                                   str(input("Ano: ")))
+            acervo.write(cadastro_livro.__str__())
+
 
 #antes fazer um texto como uma lista
 def insere_na_arvore(arquivo_path, arvore):
