095a738b163a50f499dc94d0d4d957b436a46254
Lucas1849 <lukinhas.spy6@gmail.com>
2026-07-20 12:00:22 -0300

    refac: Mudança no construtor da classe Nó para que seja usada corretamente na classe Avl importada do código da professora. Função de cadastro dos livros na árvore Avl funcionando corretamente.

--- trabalhoed2/main.py
+++ trabalhoed2/main.py
@@ -2,11 +2,12 @@ from avl import AvlTree
 from livro import Livro
 
 def main():
-    palavras = AvlTree()
+    codigos = AvlTree()
     nome_documento_livros = 'trabalhoed2/acervo.txt' #Note, aqui a escolha txt é puramente pela natureza do trabalho e indicação no campo
                                          # de entrega do moodle. Poderia ser um JSON, por exemplo.
     documentar_livro(nome_documento_livros)
-    
+    insere_na_arvore(nome_documento_livros,codigos)
+    codigos.emOrdem()
     
 #Primeiro passo é preciso estruturar como as infomações dos livros serão estruturadas para que sejam cadastrados mais de um 
 #livro por vez
@@ -22,19 +23,13 @@ def documentar_livro(arquivo_destino):
                                    str(input("Ano: ")))
             acervo.write(cadastro_livro.__str__())
 
-
-#antes fazer um texto como uma lista
+#Segundo passo seria, tá eu tenho os livros documentados em algum lugar.
+#Agora eu tenho que ler esse arquivo e pegar apenas os códigos transformando-os para inteiros
+#Na hora de devolver as informações do livro no __str__ foi utilizado ";" estrategicamente para facilitar o split()
 def insere_na_arvore(arquivo_path, arvore):
     with open(arquivo_path,'r') as arquivo:
         texto = arquivo.readlines()
-        for t in range(len(texto)):
-            arvore.insere(retorna_palavras(texto[0]))
-        
-       
-def retorna_palavras(texto_completo):
-    for i in range(len(texto_completo)):
-        for p in texto_completo[i].split(" "):
-            return p[:]
-
+        for info in texto:
+            arvore.insere(int(info.split(";")[0]))
 
 main()
--- trabalhoed2/no.py
+++ trabalhoed2/no.py
@@ -2,7 +2,7 @@ class NO:
     def __init__(self, info):
         self.info = info
         self.altura = 0
-        self.frequencia = 1
-        self.esquerda = None
-        self.direita = None
+        self.freq = 1
+        self.esq = None
+        self.dir = None
         
